2196bc4c5f8abb5abdf0ad7a0d4a671148939c94
miquel.torner.vinals <miquel.torner.vinals@estudiantat.upc.edu>
2023-01-29 19:01:21 +0100

    :'(

--- js/script.js
+++ js/script.js
@@ -19,12 +19,11 @@ function hideWindow(window) {
 
 function closeWindow(window) {
 
-  var iframe = document.getElementById("spaceshootergame");
-  iframe.setAttribute("src", "blank")
-
-  
   document.getElementById(window).style.display = "none";
-  // document.getElementById(window + "Taskbar").style.display = "none";
+  document.getElementById(window + "Taskbar").style.display = "none";
+
+  var iframe = document.getElementById("spaceshootergame");
+  iframe.remove();  
 }
 
 function minimise(window) {
